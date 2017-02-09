e45e5086d204c6551eae71cb44425a8727c30fec
zhang.Y.C <yongchao.zhang@lejent.com>
2017-02-09 13:38:31 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,13 @@
 #functional tests.
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome();
         self.browser.implicitly_wait(3) #隐式等待
 
     def tearDown(self):
@@ -13,8 +15,32 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-list', self.browser.title)
+        self.assertIn('To_Do list', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        #find_element_by_tag_name
+
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        #send_keys ， selenium 在输入框中输入内容的方法
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        #find_element_by_id
+
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
-    if __name__ == '__main__':
-        unittest.main(warnings='ignore')
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
