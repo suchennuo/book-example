1b7f312ca32fc5232de70d12eceb2ab6b9e18fb4
zhang.Y.C <yongchao.zhang@lejent.com>
2017-02-12 22:32:30 +0800

    Refactor templates to use a base template

--- function_tests/tests.py
+++ function_tests/tests.py
@@ -22,7 +22,7 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
-        self.assertIn('To_Do list', self.browser.title)
+        self.assertIn('To-Do list', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         #find_element_by_tag_name
 
