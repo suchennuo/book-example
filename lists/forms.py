83874c4195ef1b4ecce735d24dc3b5a00181e4a7
zhang.Y.C <yongchao.zhang@lejent.com>
2017-02-15 14:13:27 +0800

    New form for list items

---
+++ lists/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from lists.models import Item
+
+# class ItemForm(forms.Form):
+#     item_text = forms.CharField(
+#         widget=forms.fields.TextInput(attrs={
+#             'placeholder': 'Enter a to-do item',
+#             'class':'form-control input-lg',
+#         }),
+#     )
+
+"""
+def modelform_factory(...)
+
+Returns a ModelForm containing form fields for the given model.
+
+``fields`` is an optional list of field names. If provided, only the named
+fields will be included in the returned fields. If omitted or '__all__',
+all fields will be used.
+
+``exclude`` is an optional list of field names. If provided, the named
+fields will be excluded from the returned fields, even if they are listed
+in the ``fields`` argument.
+
+``widgets`` is a dictionary of model field names mapped to a widget.
+
+``localized_fields`` is a list of names of fields which should be localized.
+
+``formfield_callback`` is a callable that takes a model field and returns
+a form field.
+
+``labels`` is a dictionary of model field names mapped to a label.
+
+``help_texts`` is a dictionary of model field names mapped to a help text.
+
+``error_messages`` is a dictionary of model field names mapped to a
+dictionary of error messages.
+
+``field_classes`` is a dictionary of model field names mapped to a form
+field class.
+"""
+
+EMPTY_LIST_ERROR = "You can't have an empty list item"
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = { # !!! ModelForm 提供 widgets 参数
+
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_LIST_ERROR}
+        }
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from lists.forms import ItemForm
+from lists.forms import EMPTY_LIST_ERROR
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        form = ItemForm()
+        # self.fail(form.as_p()
+        # 探索性编程，看 输出的 form 缺哪些属性，然后用 meta widget attrs= 等 API 补上
+        # 为了让下面正经的 测试用例 通过
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_LIST_ERROR]
+        )
