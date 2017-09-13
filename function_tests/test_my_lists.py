faba0516464204dc78e29f57330c8c2843ffa9d3
zhang.Y.chao <yongchao1006@gmail.com>
2017-09-13 10:53:26 +0800

    finish tdd_login

--- function_tests/test_my_lists.py
+++ function_tests/test_my_lists.py
@@ -4,6 +4,8 @@ from django.contrib.sessions.backends.db import SessionStore
 from .base import FunctionTest
 User = get_user_model()
 
+
+#
 class MyListsTest(FunctionTest):
     def create_pre_authenticated_session(self, email):
         user = User.objects.create(email=email)
@@ -11,7 +13,7 @@ class MyListsTest(FunctionTest):
         session[SESSION_KEY] = user.pk
         session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
         session.save()
-        # 第一次方位设置一个 cookie
+        # 第一次访问domain， 设置一个 cookie
         # 加载 404 页面是最快的方式
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
@@ -19,3 +21,12 @@ class MyListsTest(FunctionTest):
             value=session.session_key,
             path='/'
         ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'yongchao1122@126.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email)
+
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
